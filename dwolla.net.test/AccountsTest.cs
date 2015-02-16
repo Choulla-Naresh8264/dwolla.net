f6b5edf81c28f571369f59f0c2711e3657b9b165
David Stancu <dstancu@nyu.edu>
2015-02-16 15:56:01 -0500

    added test suite

---
+++ dwolla.net.test/AccountsTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+using Dwolla;
+using Dwolla.SerializableTypes;
+
+namespace dwolla.net.test
+{
+    [TestClass]
+    public class AccountsTest
+    {
+        public Accounts a = new Accounts();
+        public JavaScriptSerializer j = new JavaScriptSerializer();
+
+        [TestMethod]
+        public void TestBasic()
+        {
+            var result = a.Basic("812-111-7219");
+            Assert.IsInstanceOfType(result, typeof(UserBasic));
+        }
+
+        [TestMethod]
+        public void TestFull()
+        {
+            var result = a.Full("J5GyKV4STxJJcVaJvdHXIOCojjEptVurmOtP8LaHk+Q8RGX6M7");
+            Assert.IsInstanceOfType(result, typeof(UserFull));
+        }    
+
+        [TestMethod]
+        public void TestBalance()
+        {
+            var result = a.Balance("J5GyKV4STxJJcVaJvdHXIOCojjEptVurmOtP8LaHk+Q8RGX6M7");
+            Assert.IsInstanceOfType(result, typeof(double));
+        }
+
+        [TestMethod]
+        public void TestNearby()
+        {
+            var result = a.Nearby(25, 25);
+            Assert.IsInstanceOfType(result, typeof(List<UserNearby>));
+        }
+
+        [TestMethod]
+        public void TestAWS()
+        {
+            var result = a.GetAutoWithdrawalStatus("J5GyKV4STxJJcVaJvdHXIOCojjEptVurmOtP8LaHk+Q8RGX6M7");
+            Assert.IsInstanceOfType(result, typeof(AutoWithdrawalStatus));
+        }
+    }
+}
