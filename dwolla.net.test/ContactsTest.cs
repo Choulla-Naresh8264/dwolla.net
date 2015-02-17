51eb5c67bd27b727de5480eca02857b3da5a0d80
David Stancu <dstancu@nyu.edu>
2015-02-17 13:53:45 -0500

    added contacts integration tests

---
+++ dwolla.net.test/ContactsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Dwolla;
+using Dwolla.SerializableTypes;
+
+namespace dwolla.net.test
+{
+    [TestClass]
+    public class ContactsTest
+    {
+        public Contacts c = new Contacts();
+        public JavaScriptSerializer j = new JavaScriptSerializer();
+
+        [TestMethod]
+        public void TestGet()
+        {
+            var result = c.Get(altToken: "J5GyKV4STxJJcVaJvdHXIOCojjEptVurmOtP8LaHk+Q8RGX6M7");
+            Assert.IsInstanceOfType(result, typeof(List<Contact>));
+        }
+
+        [TestMethod]
+        public void TestNearby()
+        {
+            var result = c.Nearby(0, 0);
+            Assert.IsInstanceOfType(result, typeof(List<UserNearby>));
+        }
+    }
+}
