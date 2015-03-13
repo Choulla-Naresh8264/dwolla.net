2fcdea5b35a2d796264d968894f3efdc18f15a73
David Stancu <dstancu@nyu.edu>
2015-03-13 09:32:42 -0400

    added home controller redirect to OAuth page

--- ExampleMVC/Controllers/HomeController.cs
+++ ExampleMVC/Controllers/HomeController.cs
@@ -4,16 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Dwolla;
+
 namespace ExampleMVC.Controllers
 {
     public class HomeController : Controller
     {
+
+        public Dwolla.OAuth o = new OAuth();
+
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Handles OAuth handshake
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult OAuth()
+        {
+            return Redirect(o.GenAuthUrl().ToString());
         }
 
     }
